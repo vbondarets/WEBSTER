8bc28b1ec1eb65700c68bbb6eb930c1c83e55a9f
Vladimir Bondarets <vbondarets@Vladimirs-MacBook-Pro.local>
2023-05-02 12:14:04 +0300

    bot fix

--- tgBot/app.js
+++ tgBot/app.js
@@ -6,22 +6,20 @@ const bot = new TelegramApi(process.env.BOT_TOKEN, {polling: true});
 bot.on('message', async (message, data, more) =>{
     const text = message.text;
     const chatId = message.chat.id;
-    console.log(message);
+    // console.log(message);
     const from = {
         name: message.from.first_name,
         id: message.from.id
     }
-    router(text, chatId, from, bot);
     await bot.sendMessage(chatId, "I got your message");
 });
 bot.on('voice', async (message, data, more) =>{
     const text = message.text;
     const chatId = message.chat.id;
-    console.log(message);
+    // console.log(message);
     const from = {
         name: message.from.first_name,
         id: message.from.id
     }
-    router(text, chatId, from, bot);
     await bot.sendMessage(chatId, "I got your voice");
 });
