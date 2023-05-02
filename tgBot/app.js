0df7dd52b4f322491bdbee469b04eb1751dc590a
Vladimir Bondarets <vbondarets@Vladimirs-MacBook-Pro.local>
2023-05-02 12:11:15 +0300

    bot voices

--- tgBot/app.js
+++ tgBot/app.js
@@ -14,3 +14,14 @@ bot.on('message', async (message, data, more) =>{
     router(text, chatId, from, bot);
     await bot.sendMessage(chatId, "I got your message");
 });
+bot.on('voice', async (message, data, more) =>{
+    const text = message.text;
+    const chatId = message.chat.id;
+    console.log(message);
+    const from = {
+        name: message.from.first_name,
+        id: message.from.id
+    }
+    router(text, chatId, from, bot);
+    await bot.sendMessage(chatId, "I got your voice");
+});
