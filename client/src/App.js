fe58e32df75c4acd98999877cf6b07fd16134f9f
root <bondaretsdirect@gmail.com>
2023-05-05 19:23:50 +0000

    some fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Stavki na sport.
         </p>
