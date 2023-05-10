0bc168bcfc3ce6d7f1d3a45b40ebe975c35ea096
root <bondaretsdirect@gmail.com>
2023-05-10 13:12:58 +0000

    fixes

--- db/01-init.sh
+++ db/01-init.sh
@@ -2,19 +2,19 @@
 set -e
 export PGPASSWORD=$POSTGRES_PASSWORD;
 psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "webster" <<-EOSQL
-  CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
-  CREATE DATABASE $APP_DB_NAME;
-  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
+  # CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+  # CREATE DATABASE $APP_DB_NAME;
+  # GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
   GRANT ALL PRIVILEGES ON DATABASE webster TO postgres;
-  \connect $APP_DB_NAME $APP_DB_USER
-  BEGIN;
-    CREATE TABLE IF NOT EXISTS event (
-	  id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
-	  aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
-	  event_data JSON NOT NULL,
-	  version INT,
-	  UNIQUE(aggregate_id, version)
-	);
-	CREATE INDEX idx_event_aggregate_id ON event (aggregate_id);
+  # \connect $APP_DB_NAME $APP_DB_USER
+  # BEGIN;
+  #   CREATE TABLE IF NOT EXISTS event (
+	#   id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
+	#   aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
+	#   event_data JSON NOT NULL,
+	#   version INT,
+	#   UNIQUE(aggregate_id, version)
+	# );
+	# CREATE INDEX idx_event_aggregate_id ON event (aggregate_id);
   COMMIT;
 EOSQL
