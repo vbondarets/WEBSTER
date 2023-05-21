17e554ccb484c92354752e8e6d6955f50219dfeb
root <root@WEBSTER-2.0>
2023-05-21 21:15:13 +0000

    bot fixes

--- tgBot/utils/downloadImage.js
+++ tgBot/utils/downloadImage.js
@@ -1,23 +1,27 @@
 const axios = require('axios');
-const {createWriteStream, createWriteStreamSync} = require('fs');
+const { createWriteStream, createWriteStreamSync } = require('fs');
 const path = require('path');
 
-const downloadImage = async(url, filename) => {
-    const fileType = url.split('.').pop();
-    const imgPath = path.resolve(__dirname, '../static/images', `${filename}.${fileType}`);
-    const {data} = await axios({
-        method: 'get',
-        url,
-        responseType: 'stream',
-    });
-    return new Promise((resolve) => {
-        const stream = createWriteStream(imgPath);
-        data.pipe(stream);
-        stream.on('finish', async() => {
-            resolve(imgPath);
-            
+const downloadImage = async (url, filename) => {
+    try {
+        const fileType = url.split('.').pop();
+        const imgPath = path.resolve(__dirname, '../static/images', `${filename}.${fileType}`);
+        const { data } = await axios({
+            method: 'get',
+            url,
+            responseType: 'stream',
+        });
+        return new Promise((resolve) => {
+            const stream = createWriteStream(imgPath);
+            data.pipe(stream);
+            stream.on('finish', async () => {
+                resolve(imgPath);
+
+            })
         })
-    })
+    } catch (error) {
+        console.log('Error: ', error.message)
+    }
 }
 
 module.exports = downloadImage
