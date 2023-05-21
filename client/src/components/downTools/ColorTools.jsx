3d137348ea69169d0acd8694d96c6397959cbdf4
Жека <jeka20001980@gmail.com>
2023-05-21 10:38:19 +0300

    optimization

--- client/src/components/downTools/ColorTools.jsx
+++ client/src/components/downTools/ColorTools.jsx
@@ -1,78 +1,62 @@
-import { Slider } from '@mui/material'
-import React, { useEffect, useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { Slider } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { setDownTool, setValues } from "../../store/reducers/ToolSlice";
 
 const ColorTools = () => {
     const colorsBar = useRef();
     const dispatch = useDispatch();
-    const { downTools } = useSelector((state) => state.toolReducer);
-    const { curDownTool } = useSelector((state) => state.toolReducer);
+    const { downTools, curDownTool } = useSelector(
+        (state) => state.toolReducer
+    );
     const [value, setValue] = useState(100);
 
-    const setActive = (e, filterValue) => {
-        // console.log(filterValue);
-        setValue(filterValue);
-        e.target.parentNode.childNodes.forEach(el => el.classList = ['colors p-2 h-fit hover:text-amber-200 hover:bg-[#333042] rounded-2xl cursor-pointer'])
-        e.target.classList = ['colors p-2 h-fit text-amber-200 rounded-2xl cursor-pointer'];
-        dispatch(setDownTool({ curDownTool: e.target.innerText }))
-        dispatch(setValues({
-            toolIndex: 2,
-            values: {
-                curSetting: e.target.innerText,
-                name: "colors",
-                filters: downTools[2].filters
-            }
-        }));
-        setValue(filterValue);
-    }
+    const setActive = (e, filter) => {
+        dispatch(setDownTool({ curDownTool: filter.name }));
+        dispatch(
+            setValues({
+                toolIndex: 2,
+                values: {
+                    curSetting: filter.name,
+                    name: "colors",
+                    filters: downTools[2].filters,
+                },
+            })
+        );
+        setValue(filter.value);
+    };
 
-    const handleChange = async (event, newValue) => {
+    const handleChange = async (e, newValue) => {
         setValue(newValue);
-        if (typeof newValue === 'number' && newValue !== null) {
-            dispatch(setValues({
+        dispatch(
+            setValues({
                 toolIndex: 2,
                 values: {
                     curSetting: curDownTool,
-                    name:"colors",
-                    filters: downTools[2].filters.map(filter => {
-                        if(filter.name === curDownTool){
-                            return({
-                                name: filter.name,
-                                value: newValue
-                            })
-                        }
-                        else {
-                            return({
-                                name: filter.name,
-                                value: filter.value
-                            })
-                        }
-                    })
-                }
-            }));
-        }
+                    name: "colors",
+                    filters: downTools[2].filters.map((filter) =>
+                        filter.name === curDownTool
+                            ? { ...filter, value: newValue }
+                            : filter
+                    ),
+                },
+            })
+        );
     };
     useEffect(() => {
-        if(downTools[2].curSetting){
-            downTools[2].filters.forEach(filter => {
-                if(filter.name === downTools[2].curSetting){
-                    setValue(filter.value)
+        if (downTools[2].curSetting) {
+            downTools[2].filters.forEach((filter) => {
+                if (filter.name === downTools[2].curSetting) {
+                    setValue(filter.value);
                 }
-            })
-            colorsBar.current.childNodes.forEach(filter => {
-                if(filter.innerText === downTools[2].curSetting){
-                    filter.classList = ['colors p-2 h-fit text-amber-200 rounded-2xl cursor-pointer']
-                }
-            })
+            });
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [downTools])
+    }, [downTools]);
 
     return (
-        <div className='colorTool'>
-            <div className='w-3/6 mx-auto'>
-                {curDownTool != null &&
+        <div className="colorTool">
+            <div className="w-3/6 mx-auto">
+                {curDownTool != null && (
                     <Slider
                         aria-label="discrete-slider"
                         value={value}
@@ -83,21 +67,31 @@ const ColorTools = () => {
                         max={200}
                         onChange={handleChange}
                     />
-                }
+                )}
             </div>
-            <div ref={colorsBar} className='colorsBar w-fit mt-1 mx-auto font-sans flex space-x-24'>
-                {downTools[2].filters.map(filter => {
-                    return <p
-                        key={filter.name}
-                        className='colors p-2 h-fit hover:text-amber-200 hover:bg-[#333042] rounded-2xl cursor-pointer'
-                        onClick={(e) => { setActive(e, filter.value) }}
-                    >
-                        {filter.name}
-                    </p>
+            <div
+                ref={colorsBar}
+                className="colorsBar w-fit mt-1 mx-auto font-sans flex space-x-24"
+            >
+                {downTools[2].filters.map((filter) => {
+                    return (
+                        <p
+                            key={filter.name}
+                            className={`${
+                                filter.name === downTools[2].curSetting &&
+                                "text-amber-200 border border-mainFontColor"
+                            } colors p-2 h-fit hover:text-amber-200 hover:bg-[#333042] rounded-2xl cursor-pointer`}
+                            onClick={(e) => {
+                                setActive(e, filter);
+                            }}
+                        >
+                            {filter.name}
+                        </p>
+                    );
                 })}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ColorTools
+export default ColorTools;
--- client/src/components/downTools/CutTools.jsx
+++ client/src/components/downTools/CutTools.jsx
@@ -9,19 +9,13 @@ const CutTools = () => {
     const { downTools } = useSelector((state) => state.toolReducer);
     const dispatch = useDispatch();
 
-    const chooseHandler = (e, format) => {
-        e.currentTarget.parentNode.childNodes.forEach(el => el.classList = [`${format} format h-5/6 w-20 hover:text-amber-200 hover:bg-[#333042] rounded-2xl cursor-pointer flex flex-col justify-center items-center content-center`])
-        e.currentTarget.classList = [`${format} formats h-5/6 w-20 text-amber-200  border border-mainFontColor rounded-2xl cursor-pointer flex flex-col justify-center items-center content-center`]
-        dispatch(setFormat({ format }))
-    }
     return (
         <div ref={cutBar} className='formatTool w-fit h-full mt-1 font-sans flex flex-row space-x-6 mx-auto'>
             {downTools[0].formats.map(format => {
-                if(format === downTools[0].curFormat){
                     return <div
                         key={format}
-                        className={`${format} formats h-5/6 w-20 text-amber-200  border border-mainFontColor rounded-2xl cursor-pointer flex flex-col justify-center items-center content-center`}
-                        onClick={(e) => { chooseHandler(e, format) }}
+                        className={`${format} ${format === downTools[0].curFormat && "text-amber-200 border border-mainFontColor"} format h-5/6 w-20 hover:text-amber-200 hover:bg-[#333042] rounded-2xl cursor-pointer flex flex-col justify-center items-center content-center`}
+                        onClick={(e) => { dispatch(setFormat({ format })) }}
                     >
                         {format === 'Custom'
                             ?
@@ -35,26 +29,6 @@ const CutTools = () => {
                         }
                         <p className='mb-1'>{format}</p>
                     </div>
-                }
-                else {
-                    return <div
-                        key={format}
-                        className={`${format} format h-5/6 w-20 hover:text-amber-200 hover:bg-[#333042] rounded-2xl cursor-pointer flex flex-col justify-center items-center content-center`}
-                        onClick={(e) => { chooseHandler(e, format) }}
-                    >
-                        {format === 'Custom'
-                            ?
-                            <div className='iconContainer w-fit h-2/3 mt-1'>
-                                <CheckBoxOutlineBlankRoundedIcon />
-                            </div>
-                            :
-                            <div className='iconContainer w-fit h-2/3 mt-1'>
-                                <ContentCutRoundedIcon />
-                            </div>
-                        }
-                        <p className='mb-1'>{format}</p>
-                    </div>
-                }
             })}
         </div>
     )
