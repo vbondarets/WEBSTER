4ea86de5701241a7d1a48249815e1038e861ca1f
Жека <jeka20001980@gmail.com>
2023-05-29 10:51:59 +0300

    design

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,118 +1,165 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const AboutPage = () => {
     return (
-        <div>
-            <div className="text-center flex content-center">
-                <h1 className="text-3xl mt-2 m-auto w-6/12 font-bold border-b pb-2">About Us</h1>
-            </div>
-            <div className="flex content-center items-center mt-5 justify-center">
-                <div className="w-1/3 bg-mainFontColor h-64 mr-6 p-10 rounded-xl shadow-lg shadow-mainFontColor/50">
-                    <h2 className="text-white text-xl border-b border-white font-bold">Main Mission</h2>
-                    <p className="text-white">Our <b>main mission</b> was to create a fast and feature-rich application that can do basic photo editing with ease. 
-                        This includes features such as photo cropping, applying filters, editing, basic photo transformation, and adding text to photos. 
-                        It's all included in our app. Enjoy using it!</p>
-                </div>
-                <div className="w-1/3 bg-mainFontColor h-64 p-10 rounded-xl shadow-lg shadow-mainFontColor/50">
-                    <h2 className="text-white text-xl border-b border-white font-bold">Our Vision</h2>
-                    <p className="text-white">What is our vision for a modern photo editing app? 
-                        First of all, this is a fast application, according to the legend of which you can do almost everything in a row. 
-                        Secondly, it is something easy to perceive on an intuitive level. 
-                        And last but not least, it's a multi-functional app where you can do whatever you want with your photos!</p>
-                </div>
-            </div>
-            <div className="flex items-center justify-center mt-4 flex-col">
-                <div className="mb-6 w-1/2 h-60 bg-mainFontColor p-10 rounded-xl shadow-lg shadow-mainFontColor/50">
-                    <h2 className="text-xl font-bold text-white mb-4 border-b">Our Team</h2>
-                    <div className="flex mb-3 justify-between">
-                        <Link to="https://github.com/determa" target="_blank"
-                            className="flex flex-col items-center">
-                            <img className="w-28 h-28 rounded-full" src="https://lms.khpi.ucode-connect.study/media/profile_photo/ybobrov.png" alt="profile_photo"></img>
-                            <p className="text-white">YBOBROV</p>
-                        </Link>
-                        <Link to="https://github.com/Spoty-O" target="_blank" 
-                            className="flex flex-col items-center">
-                            <img className="w-28 h-28 rounded-full" src="https://lms.khpi.ucode-connect.study/media/profile_photo/dkorotysh.png" alt="profile_photo"></img>
-                            <p className="text-white">DKOROTYSH</p>
-                        </Link>
-                        <Link to="https://github.com/vbondarets" target="_blank" 
-                            className="flex flex-col items-center">
-                            <img className="w-28 h-28 rounded-full" src="https://avatars.githubusercontent.com/u/90688082?v=4" alt="profile_photo"></img>
-                            <p className="text-white">VBONDARETS</p>
-                        </Link>
-                        <Link to="https://github.com/estfo" target="_blank"
-                            className="flex flex-col items-center">
-                            <img className="w-28 h-28 rounded-full" src="https://lms.khpi.ucode-connect.study/media/profile_photo/idashchenk.png" alt="profile_photo"></img>
-                            <p className="text-white">IDASHCHENK</p>
-                        </Link>
+        <div className="flex flex-col w-full justify-center items-center">
+            <div className="flex flex-col mt-2 mb-6 gap-8 justify-center items-center text-white w-full max-w-7xl">
+                <h1 className="text-3xl w-1/2 font-bold border-b pb-2 text-center">
+                    About Us
+                </h1>
+                <div className="grid grid-cols-1 w-full px-6 md:w-2/3 lg:grid-cols-2 gap-6 items-center justify-center">
+                    <div className="bg-mainFontColor w-hull h-full p-10 rounded-xl shadow-lg shadow-mainFontColor/50">
+                        <h2 className="text-2xl border-b border-white pb-1 mb-3 font-bold">
+                            Main Mission
+                        </h2>
+                        <p>
+                            Our <strong>main mission</strong> was to create a
+                            fast and feature-rich application that can do basic
+                            photo editing with ease. This includes features such
+                            as photo cropping, applying filters, editing, basic
+                            photo transformation, and adding text to photos.
+                            It's all included in our app. Enjoy using it!
+                        </p>
+                    </div>
+                    <div className="bg-mainFontColor w-hull h-full p-10 rounded-xl shadow-lg shadow-mainFontColor/50">
+                        <h2 className="text-2xl border-b border-white pb-1 mb-3 font-bold">
+                            Our Vision
+                        </h2>
+                        <p>
+                            What is our vision for a modern photo editing app?
+                            First of all, this is a fast application, according
+                            to the legend of which you can do almost everything
+                            in a row. Secondly, it is something easy to perceive
+                            on an intuitive level. And last but not least, it's
+                            a multi-functional app where you can do whatever you
+                            want with your photos!
+                        </p>
                     </div>
                 </div>
-                <div className="bg-mainFontColor h-52 p-10 rounded-xl w-1/2 shadow-lg shadow-mainFontColor/50">
-                    <h2 className="text-white font-bold text-2xl border-b mb-10">Stack Of Technologies</h2>
-                    <div className="flex items-center justify-between">
-                        <div className="flex flex-col items-center text-white text-xl">
-                            <img 
-                                className="w-10 h-10"
-                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" 
-                                alt="React" 
-                            />
-                            <span>React</span>
+                <div className="flex flex-col w-full px-6 md:w-2/3 lg:w-1/2 items-center justify-center">
+                    <div className="mb-6 bg-mainFontColor w-full p-10 rounded-xl shadow-lg shadow-mainFontColor/50">
+                        <h2 className="text-2xl border-b border-white pb-1 mb-6 font-bold">
+                            Our Team
+                        </h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3 text-xl">
+                            <Link
+                                to="https://github.com/determa"
+                                target="_blank"
+                                className="flex flex-col items-center"
+                            >
+                                <img
+                                    className="w-full h-full max-w-[150px] rounded-full"
+                                    src="https://lms.khpi.ucode-connect.study/media/profile_photo/ybobrov.png"
+                                    alt="profile_photo"
+                                ></img>
+                                <p>YBOBROV</p>
+                            </Link>
+                            <Link
+                                to="https://github.com/Spoty-O"
+                                target="_blank"
+                                className="flex flex-col items-center"
+                            >
+                                <img
+                                    className="w-full h-full max-w-[150px] rounded-full"
+                                    src="https://lms.khpi.ucode-connect.study/media/profile_photo/dkorotysh.png"
+                                    alt="profile_photo"
+                                ></img>
+                                <p>DKOROTYSH</p>
+                            </Link>
+                            <Link
+                                to="https://github.com/vbondarets"
+                                target="_blank"
+                                className="flex flex-col items-center"
+                            >
+                                <img
+                                    className="w-full h-full max-w-[150px] rounded-full"
+                                    src="https://avatars.githubusercontent.com/u/90688082?v=4"
+                                    alt="profile_photo"
+                                ></img>
+                                <p>VBONDARETS</p>
+                            </Link>
+                            <Link
+                                to="https://github.com/estfo"
+                                target="_blank"
+                                className="flex flex-col items-center"
+                            >
+                                <img
+                                    className="w-full h-full max-w-[150px] rounded-full"
+                                    src="https://lms.khpi.ucode-connect.study/media/profile_photo/idashchenk.png"
+                                    alt="profile_photo"
+                                ></img>
+                                <p>IDASHCHENK</p>
+                            </Link>
                         </div>
-                        <div className="flex flex-col items-center text-white text-xl">
-                            <img 
-                                className="w-10 h-10"
-                                src="https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png   " 
-                                alt="RTK" 
-                            />
-                            <span>RTK</span>
-                        </div>
-                        <div className="flex flex-col items-center text-white text-xl">
-                            <img 
-                                className="w-10 h-10"
-                                src="https://camo.qiitausercontent.com/44049127109ccf907500dba6026b174154388cb0/68747470733a2f2f71696974612d696d6167652d73746f72652e73332e616d617a6f6e6177732e636f6d2f302f37353633332f38323938633065652d636430302d653634362d303465372d6338393264323165653566642e706e67" 
-                                alt="JOI" 
-                            />
-                            <span>JOI</span>
-                        </div>
-                        <div className="flex flex-col items-center text-white text-xl">
-                            <img 
-                                className="w-10 h-10"
-                                src="https://mui.com/static/logo.png" 
-                                alt="MUI" 
-                            />
-                            <span>MUI</span>
-                        </div>
-                        <div className="flex flex-col items-center text-white text-xl">
-                            <img 
-                                className="w-20 h-10"
-                                src="https://ies.solutions/wordpress/wp-content/uploads/jwt.png" 
-                                alt="JWT" 
-                            />
-                            <span>JWT</span>
-                        </div>
-                        <div className="flex flex-col items-center text-white text-xl">
-                            <img 
-                                className="w-10 h-10"
-                                src="https://mmg-gitlab.fjfi.cvut.cz/gitlab/uploads/-/system/project/avatar/6/tnl_logo_circle.png" 
-                                alt="TNL" 
-                            />
-                            <span>TNL</span>
-                        </div>
-                        <div className="flex flex-col items-center text-white text-xl">
-                            <img 
-                                className="w-30 h-10"
-                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/OpenAI_Logo.svg/2560px-OpenAI_Logo.svg.png" 
-                                alt="OpenAI" 
-                            />
-                            <span>OpenAI</span>
+                    </div>
+                    <div className="mb-6 bg-mainFontColor w-full p-10 rounded-xl shadow-lg shadow-mainFontColor/50">
+                        <h2 className="text-2xl border-b border-white pb-1 mb-6 font-bold">
+                            Stack Of Technologies
+                        </h2>
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-5 items-center">
+                            <div className="flex flex-col items-center text-xl">
+                                <img
+                                    className="h-12"
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+                                    alt="React"
+                                />
+                                <span>React</span>
+                            </div>
+                            <div className="flex flex-col items-center text-xl">
+                                <img
+                                    className="h-12"
+                                    src="https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png   "
+                                    alt="RTK"
+                                />
+                                <span>RTK</span>
+                            </div>
+                            <div className="flex flex-col items-center text-xl">
+                                <img
+                                    className="h-12"
+                                    src="https://camo.qiitausercontent.com/44049127109ccf907500dba6026b174154388cb0/68747470733a2f2f71696974612d696d6167652d73746f72652e73332e616d617a6f6e6177732e636f6d2f302f37353633332f38323938633065652d636430302d653634362d303465372d6338393264323165653566642e706e67"
+                                    alt="JOI"
+                                />
+                                <span>JOI</span>
+                            </div>
+                            <div className="flex flex-col items-center text-xl">
+                                <img
+                                    className="h-12"
+                                    src="https://mui.com/static/logo.png"
+                                    alt="MUI"
+                                />
+                                <span>MUI</span>
+                            </div>
+                            <div className="flex flex-col items-center text-xl">
+                                <img
+                                    className="w-20 h-10"
+                                    src="https://ies.solutions/wordpress/wp-content/uploads/jwt.png"
+                                    alt="JWT"
+                                />
+                                <span>JWT</span>
+                            </div>
+                            <div className="flex flex-col items-center text-xl">
+                                <img
+                                    className="h-12"
+                                    src="https://mmg-gitlab.fjfi.cvut.cz/gitlab/uploads/-/system/project/avatar/6/tnl_logo_circle.png"
+                                    alt="TNL"
+                                />
+                                <span>TNL</span>
+                            </div>
+                            <div className="flex flex-col items-center text-xl">
+                                <img
+                                    className="w-30 h-10"
+                                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/OpenAI_Logo.svg/2560px-OpenAI_Logo.svg.png"
+                                    alt="OpenAI"
+                                />
+                                <span>OpenAI</span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default AboutPage;
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -9,59 +9,66 @@ import TextTutor from '../assets/Instructions/Text.gif'
 import DownloadTutor from '../assets/Instructions/Download.gif'
 
 const MainPage = () => {
-  return (
-    <div className='flex flex-col mt-4 items-center'>
-      <div className='flex justify-center w-1/2 bg-mainFontColor text-white rounded-xl p-5 shadow-lg shadow-blue-400/50'>
-        <h1 className='text-2xl font-bold border-b w-full text-center'>INSTRUCTION</h1>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p>With the help of the <b>Webster</b> program, you can do all kinds of manipulations with the processing and adjustment of photos. So how to use this program?</p>
-        <p><b>1.</b> You need to click on the button "Get Started" or click on "Studio" on Navbar, that will take you to the studio window.</p>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>2.</b> In the studio, you need to transfer the photo you want to process or select a photo from your PC.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={AddFileTutorial} alt="Gif"/>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>3.</b> In the studio, you can crop your image and apply this operation.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={CutTutor} alt="Gif"/>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>4.</b> In the studio, you can apply filters on your image and apply this operation.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={CutTutor} alt="Gif"/>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>5.</b> With help of "Color" you are able to change brightness, contrast and other setting of your photo.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={ColorTutor} alt="Gif"/>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>6.</b> With help of "Draw" you are able to draw smth on your photo.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={ColorTutor} alt="Gif"/>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>7.</b> Also you can rotate your image and apply this operation.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={RotateTutor} alt="Gif"/>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>8.</b> With hepl of "Text" you can write smth on your image and save result.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={TextTutor} alt="Gif"/>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>9.</b> Resize give you opportunity to change current size of your image.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={ColorTutor} alt="Gif"/>
-      </div>
-      <div className='flex justify-center flex-col mt-4 w-1/2 text-xl bg-mainFontColor text-white p-4 rounded-xl shadow-lg shadow-blue-400/50'>
-        <p><b>10.</b> You can easily save all your changes and download new image with our last futures.</p>
-        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={DownloadTutor} alt="Gif"/>
-      </div>
-      <Link
-        to='/studio'
-        className='p-5 mt-6 bg-indigo-500 rounded-full w-48 text-center shadow-lg shadow-indigo-500/50 text-white ease-in-out duration-300 text-lg hover:bg-mainFontColor hover:shadow-blue-400/50'
-      >
-        Get Started <StartIcon /> 
-      </Link>
-    </div>
-  )
+    return (
+        <div className='flex flex-col w-full justify-center items-center'>
+            <div className='flex flex-col mt-2 mb-6 gap-2 justify-center items-center text-white w-full max-w-7xl'>
+                {/* <div className='flex justify-center w-1/2 bg-mainFontColor rounded-xl p-5 shadow-lg shadow-blue-400/50'>
+                    <h1 className='text-2xl font-bold border-b w-full pb-1 text-center'>INSTRUCTION</h1>
+                </div> */}
+                <h1 className="text-3xl w-full sm:w-1/2 font-bold border-b pb-2 text-center">
+                    INSTRUCTION
+                </h1>
+                <div className='flex flex-col w-full px-6 md:w-2/3 gap-4 text-xl'>
+                    <div className='mt-3 flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p>With the help of the <strong>Webster</strong> program, you can do all kinds of manipulations with the processing and adjustment of photos. So how to use this program?</p>
+                        <p><strong>1.</strong> You need to click on the button "Get Started" or click on "Studio" on Navbar, that will take you to the studio window.</p>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>2.</strong> In the studio, you need to transfer the photo you want to process or select a photo from your PC.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={AddFileTutorial} alt="Gif"/>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>3.</strong> In the studio, you can crop your image and apply this operation.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={CutTutor} alt="Gif"/>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>4.</strong> In the studio, you can apply filters on your image and apply this operation.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={CutTutor} alt="Gif"/>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>5.</strong> With help of "Color" you are able to change brightness, contrast and other setting of your photo.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={ColorTutor} alt="Gif"/>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>6.</strong> With help of "Draw" you are able to draw smth on your photo.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={ColorTutor} alt="Gif"/>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>7.</strong> Also you can rotate your image and apply this operation.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={RotateTutor} alt="Gif"/>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>8.</strong> With hepl of "Text" you can write smth on your image and save result.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={TextTutor} alt="Gif"/>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>9.</strong> Resize give you opportunity to change current size of your image.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={ColorTutor} alt="Gif"/>
+                    </div>
+                    <div className='flex justify-center flex-col bg-mainFontColor p-6 rounded-xl shadow-lg shadow-blue-400/50 gap-3'>
+                        <p><strong>10.</strong> You can easily save all your changes and download new image with our last futures.</p>
+                        <img className='mt-2 mb-2 rounded-xl shadow-lg shadow-indigo-500/50' src={DownloadTutor} alt="Gif"/>
+                    </div>
+                    <Link
+                        to='/studio'
+                        className='p-5 mt-6 bg-indigo-500 rounded-full w-48 text-center shadow-lg shadow-indigo-500/50 ease-in-out duration-300 text-lg hover:bg-mainFontColor hover:shadow-blue-400/50'
+                    >
+                        Get Started <StartIcon /> 
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default MainPage;
