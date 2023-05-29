cf2f13e6dcc4e967f4f8a37c50176a43c77b95db
Vladimir Bondarets <vbondarets@Vladimirs-MacBook-Pro.local>
2023-05-29 19:40:39 +0300

    fix

--- client/src/components/ModalDownload.jsx
+++ client/src/components/ModalDownload.jsx
@@ -57,6 +57,14 @@ const ModalDownload = (props) => {
                 width: width,
                 height: height
             });
+            template.style.filter = `${downTools[2].filters[0].name}(${downTools[2].filters[0].value}%) 
+                ${downTools[2].filters[1].name}(${downTools[2].filters[1].value}%) 
+                ${downTools[2].filters[2].name}(${downTools[2].filters[2].value}%)
+                ${downTools[2].filters[3].name}(${downTools[2].filters[3].value}%)
+                ${downTools[2].filters[4].name}(${downTools[2].filters[4].value}px)
+                ${downTools[2].filters[5].name}(${downTools[2].filters[5].value}%)
+                ${downTools[2].filters[6].name}(${downTools[2].filters[6].value}%)
+                ${downTools[2].filters[7].name}(${downTools[2].filters[7].value}deg)`;
             fabric.Image.fromURL(imgHref, function (img) {
                 let scaleX = width / img.width;
                 let scaleY = height / img.height;
@@ -66,14 +74,6 @@ const ModalDownload = (props) => {
                 if (height < img.height) {
                     scaleY = img.height / height
                 }
-                img.style.filter = `${downTools[2].filters[0].name}(${downTools[2].filters[0].value}%) 
-                ${downTools[2].filters[1].name}(${downTools[2].filters[1].value}%) 
-                ${downTools[2].filters[2].name}(${downTools[2].filters[2].value}%)
-                ${downTools[2].filters[3].name}(${downTools[2].filters[3].value}%)
-                ${downTools[2].filters[4].name}(${downTools[2].filters[4].value}px)
-                ${downTools[2].filters[5].name}(${downTools[2].filters[5].value}%)
-                ${downTools[2].filters[6].name}(${downTools[2].filters[6].value}%)
-                ${downTools[2].filters[7].name}(${downTools[2].filters[7].value}deg)`;
                 newCanvas.setBackgroundImage(img, newCanvas.renderAll.bind(newCanvas), {
                     scaleX: scaleX,
                     scaleY: scaleY,
